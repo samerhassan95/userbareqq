b80b86c063ba673d896d708f8fc3805cc8109c9a
samerhassan95 <arch.samerhassan@gmail.com>
2026-05-20 16:45:32 +0300

    Improve ProductStrategyTipResource with more details

--- app/Http/Resources/ProductStrategyTipResource.php
+++ app/Http/Resources/ProductStrategyTipResource.php
@@ -11,8 +11,13 @@ class ProductStrategyTipResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'product_id' => $this->product_id,
             'text' => $this->text,
             'platforms' => $this->platforms ?? [],
+            'platform_names' => collect($this->platforms ?? [])->map(fn($p) => ucfirst($p))->toArray(),
+            'sort_order' => $this->sort_order ?? 0,
+            'created_at' => $this->created_at?->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at?->format('Y-m-d H:i:s'),
         ];
     }
 }
