0eba648b414d08933cd5e758bccf672edbe877b0
samerhassan95 <arch.samerhassan@gmail.com>
2026-05-25 11:43:54 +0300

    client approve

--- routes/posts.php
+++ routes/posts.php
@@ -16,23 +16,23 @@ Route::middleware('admin')->prefix('admin')->group(function () {
     Route::delete('posts/{id}', [AdminPostController::class, 'destroy']);
 });
 
-// Marketer Routes (Create & Edit) - Client with role=marketer
-Route::middleware(['auth:api', 'check.role:marketer'])->prefix('marketer')->group(function () {
+// Marketer Routes (Create & Edit) - Marketer users
+Route::middleware(['auth:marketer'])->prefix('marketer')->group(function () {
     Route::get('posts', [MarketerPostController::class, 'index']);
     Route::post('posts', [MarketerPostController::class, 'store']);
     Route::put('posts/{id}', [MarketerPostController::class, 'update']);
     Route::post('posts/{id}', [MarketerPostController::class, 'update']); // For form-data
 });
 
-// Designer Routes (Edit only) - Client with role=designer
-Route::middleware(['auth:api', 'check.role:designer'])->prefix('designer')->group(function () {
+// Designer Routes (Edit only) - Designer users
+Route::middleware(['auth:designer'])->prefix('designer')->group(function () {
     Route::get('posts', [DesignerPostController::class, 'index']);
     Route::put('posts/{id}', [DesignerPostController::class, 'update']);
     Route::post('posts/{id}', [DesignerPostController::class, 'update']); // For form-data
 });
 
 // Client Routes (View, Feedback, Approve) - Regular clients
-Route::middleware(['auth:api', 'check.role:client'])->prefix('client')->group(function () {
+Route::middleware(['client'])->prefix('client')->group(function () {
     Route::get('posts', [ClientPostController::class, 'index']);
     Route::get('posts/{id}', [ClientPostController::class, 'show']);
     Route::post('posts/{id}/feedback', [ClientPostController::class, 'addFeedback']);
